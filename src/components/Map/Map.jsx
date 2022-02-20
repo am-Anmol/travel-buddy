b0c1c625f595e87d45cb7c061c6b313440db088a
Anmol Gupta <guptaanmol248001@gmail.com>
2022-02-20 13:48:38 +0530

    Added map basic layout

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab/Rating';
 
-const Map = () => {
-    return(
-        <div>
-            <h1>Map</h1>
-        </div>
+// import mapStyles from '../../mapStyles';
+import useStyles from './styles.js';
+
+const Map = ({ coords, places, setCoords, setBounds, setChildClicked, weatherData }) => {
+  const matches = useMediaQuery('(min-width:600px)');
+  const classes = useStyles();
+  const coordinates = { lat: 0, lng: 0 };
+
+  return (
+    <div className={classes.mapContainer}>
+        <GoogleMapReact
+            bootstrapURLKeys={{ key: 'AIzaSyBdDGM0M-nLe3FWpdMCUPRoCDPMpJV_amM' }}
+            defaultCenter={coordinates}
+            center={coordinates}
+            defaultZoom={14}
+            margin={[50, 50, 50, 50]}
+            options={''}
+            onChange={''}
+            onChildClick={''}
+        >
+        </GoogleMapReact>
+    </div>
     );
-}
+};
 
 export default Map;
